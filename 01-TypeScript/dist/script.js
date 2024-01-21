8a84aff0702b7c785054c122aeba68abcf6b41b5
Eleilson-Dev <maxta.gamer@gmail.com>
2024-01-21 13:40:03 -0300

    update

--- 01-TypeScript/dist/script.js
+++ 01-TypeScript/dist/script.js
@@ -1,2 +1,23 @@
-export let age = 5;
-console.log(age);
+// Tipos basicos
+let age = 5;
+const fistName = 'Elleylson';
+const isValid = true;
+const ids = [1, 2, 3, 4];
+const booleans = [true, false, true, false];
+const strings = ['Elleylson', 'santtos'];
+// Tupla
+const person = [1, 'Eleilson'];
+// Lista de tuplas
+export const people = [
+    [1, 'str1'],
+    [2, 'str2'],
+    [3, 'str3'],
+];
+// Intersections
+const produtcId = '1';
+// Enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+})(Direction || (Direction = {}));
--- 01-TypeScript/src/script.ts
+++ 01-TypeScript/src/script.ts
@@ -1,3 +1,27 @@
-export let age: number = 5;
+// Tipos basicos
+let age: number = 5;
+const fistName: string = 'Elleylson';
+const isValid: boolean = true;
 
-console.log(age);
+const ids: number[] = [1, 2, 3, 4];
+const booleans: boolean[] = [true, false, true, false];
+const strings: string[] = ['Elleylson', 'santtos'];
+
+// Tupla
+const person: [number, string] = [1, 'Eleilson'];
+
+// Lista de tuplas
+export const people: [number, string][] = [
+  [1, 'str1'],
+  [2, 'str2'],
+  [3, 'str3'],
+];
+
+// Intersections
+const produtcId: string | number = '1';
+
+// Enum
+enum Direction {
+  Up = 1,
+  Down = 2,
+}
